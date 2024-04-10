fc895588223ba2036190056477b46700d953b7e0
Monica <155568667+grinxy@users.noreply.github.com>
2024-04-10 14:53:50 +0200

    project creation

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+//v1 of the API --> future versions should not affect apps using this set up
+
+// Version 1 of the API
+Route::prefix('v1')->group(function () {
+    // Open routes (no authentication required)
+    Route::post('register', [ApiController::class, 'register']);
+    Route::post('login', [ApiController::class, 'login']);
+
+    // Protected routes (require authentication)
+    Route::middleware('auth:api')->group(function () {
+        Route::get('profile', [ApiController::class, 'profile']);
+        Route::get('logout', [ApiController::class, 'logout']);
+    });
+});
