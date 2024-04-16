d27eea36fdc42ab3c2e2a52eb920d8db344cc657
Monica <155568667+grinxy@users.noreply.github.com>
2024-04-16 10:52:33 +0200

    funciones jugada con resultado para un usuario

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'dice1_value',
+        'dice2_value',
+        'sum',
+        'result'
+    ];
+
 }
