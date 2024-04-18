d53279b56523d5f7dcf5db0550683a2705152811
Monica <155568667+grinxy@users.noreply.github.com>
2024-04-18 17:03:44 +0200

    cambios revertidos a punto partida testing

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -4,35 +4,18 @@ namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Artisan;
 
 trait CreatesApplication
 {
     /**
      * Creates the application.
      */
-    /*    public function createApplication(): Application
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        return $app;
-    }*/
-
     public function createApplication(): Application
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
-        $this->clearConfigCache();
-
         return $app;
-    }
-
-    private function clearConfigCache(): void
-    {
-        Artisan::call('config:clear');
     }
 }
